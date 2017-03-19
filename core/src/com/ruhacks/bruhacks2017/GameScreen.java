208a0332afd4309ef9f2742fc818675774483edd
murizvi <rizvmu@gmail.com>
2017-03-18 20:46:26 -0400

    Actually fix merge conflict

--- core/src/com/ruhacks/bruhacks2017/GameScreen.java
+++ core/src/com/ruhacks/bruhacks2017/GameScreen.java
@@ -104,66 +104,8 @@ public class GameScreen extends AbstractScreen  {
     }*/
 
     @Override
-    public void update() {
-        rainManager.update();
-        lighteningCounter--;
-        if (lighteningCounter == 10) {
-            lighteningImage.setColor(1.0f, 1.0f, 1.0f, 1.0f);
-            lighteningAlpha = 1.0f;
-        } else if (lighteningCounter == 0) {
-            lighteningImage.setColor(1.0f, 1.0f, 1.0f, 1.0f);
-            lighteningAlpha = 1.0f;
-            lighteningCounter = (int)(Math.random() * 100 + 150);
-        }
-
-        if (lighteningAlpha > 0) {
-            lighteningAlpha -= 0.05f;
-            if (lighteningAlpha < 0) {
-                lighteningAlpha = 0;
-            }
-            lighteningImage.setColor(1.0f, 1.0f, 1.0f, lighteningAlpha);
-        }
-
-        if (smokeCounter <= 0) {
-            smokeCounter = 20;
-            Image smoke = new Image(squareTexture);
-            smoke.setSize(20f, 20f);
-            if (player.getScaleX() > 0) {
-                smoke.setX(player.getX() + player.getWidth() / 2f - (player.getWidth() / 2f + 9f - 20f / 2f) * (float) Math.cos(Math.toRadians(player.getRotation())) + (player.getHeight() / 2f + 20f) * (float) Math.sin(Math.toRadians(player.getRotation())));
-            } else {
-                smoke.setX(player.getX() + player.getWidth() / 2f + ((player.getWidth() / 2f + 9f - 20f / 2f) * (float) Math.cos(Math.toRadians(player.getRotation())) + (player.getHeight() / 2f + 20f) * (float) Math.sin(Math.toRadians(player.getRotation()))));
-            }
-            smoke.setY(player.getY() + player.getHeight() / 2f - (player.getHeight() / 2f + 20f) * (float)Math.cos(Math.toRadians(player.getRotation())) + (player.getWidth() / 2f + 9f - 20f / 2f) * (float)Math.sin(Math.toRadians(player.getRotation())));
-            smokeList.add(smoke);
-            this.addActor(smoke);
-            smokeAlpha.add(0f);
-        }
-
-        for (int i = 0; i < smokeAlpha.size(); ++i) {
-            Float a = smokeAlpha.get(i) - 0.05f;
-            smokeAlpha.remove(i);
-            smokeAlpha.add(i, a);
-            smokeList.get(i).setColor(0.2f, 0.2f, 0.2f, a);
-        }
-
-        for (Image smoke : smokeList) {
-            float size = smoke.getWidth() - 0.75f;
-            smoke.setSize(size, size);
-            smoke.setOrigin(size / 2f, size / 2f);
-            smoke.setX(smoke.getX() + 0.005f);
-            smoke.setY(smoke.getY() + 0.005f);
-            smoke.rotateBy((float)Math.toDegrees(Math.PI / 2f + Math.PI / 2f * Math.random()));
-        }
-
-        if (!smokeList.isEmpty() && smokeList.get(0).getWidth() < 0.51f) {
-            smokeList.get(0).remove();
-            smokeList.remove(0);
-            smokeAlpha.remove(0);
-        }
-
-
     public void update() throws IOException {
-	float xMove = (targetX - player.getX()) / 15f;
+        float xMove = (targetX - player.getX()) / 15f;
         float yMove = (targetY - player.getY()) / 15f;
         float speed = (float)Math.sqrt(xMove * xMove + yMove * yMove);
         smokeCounter -= speed;
@@ -187,20 +129,64 @@ public class GameScreen extends AbstractScreen  {
         player.setY(player.getY() + yMove );
 
         player.update(speed);
-        player.update();
-        //System.out.println(client.getPosition()[0]);
-        /*
-        if (player.getY() <= platformObject.getY() + platformObject.getHeight() &&
-                player.getX() + player.getWidth() > platformObject.getX() &&
-                player.getX() < platformObject.getX() + platformObject.getWidth()) {
-            player.setY(platformObject.getY() + platformObject.getHeight());
-            player.setSpeed(-0.8f * player.getSpeed());
-            if (Math.abs(player.getSpeed()) < 0.11f * UNIT) {
-                player.stopFall();
-            }
+
+        rainManager.update();
+        lighteningCounter--;
+        if (lighteningCounter == 10) {
+            lighteningImage.setColor(1.0f, 1.0f, 1.0f, 1.0f);
+            lighteningAlpha = 1.0f;
+        } else if (lighteningCounter == 0) {
+            lighteningImage.setColor(1.0f, 1.0f, 1.0f, 1.0f);
+            lighteningAlpha = 1.0f;
+            lighteningCounter = (int) (Math.random() * 100 + 150);
         }
-        */
+
+        if (lighteningAlpha > 0) {
+            lighteningAlpha -= 0.05f;
+            if (lighteningAlpha < 0) {
+                lighteningAlpha = 0;
+            }
+            lighteningImage.setColor(1.0f, 1.0f, 1.0f, lighteningAlpha);
+        }
+
+        if (smokeCounter <= 0) {
+            smokeCounter = 20;
+            Image smoke = new Image(squareTexture);
+            smoke.setSize(20f, 20f);
+            if (player.getScaleX() > 0) {
+                smoke.setX(player.getX() + player.getWidth() / 2f - (player.getWidth() / 2f + 9f - 20f / 2f) * (float) Math.cos(Math.toRadians(player.getRotation())) + (player.getHeight() / 2f + 20f) * (float) Math.sin(Math.toRadians(player.getRotation())));
+            } else {
+                smoke.setX(player.getX() + player.getWidth() / 2f + ((player.getWidth() / 2f + 9f - 20f / 2f) * (float) Math.cos(Math.toRadians(player.getRotation())) + (player.getHeight() / 2f + 20f) * (float) Math.sin(Math.toRadians(player.getRotation()))));
+            }
+            smoke.setY(player.getY() + player.getHeight() / 2f - (player.getHeight() / 2f + 20f) * (float) Math.cos(Math.toRadians(player.getRotation())) + (player.getWidth() / 2f + 9f - 20f / 2f) * (float) Math.sin(Math.toRadians(player.getRotation())));
+            smokeList.add(smoke);
+            this.addActor(smoke);
+            smokeAlpha.add(0f);
+        }
+
+        for (int i = 0; i < smokeAlpha.size(); ++i) {
+            Float a = smokeAlpha.get(i) - 0.05f;
+            smokeAlpha.remove(i);
+            smokeAlpha.add(i, a);
+            smokeList.get(i).setColor(0.2f, 0.2f, 0.2f, a);
+        }
+
+        for (Image smoke : smokeList) {
+            float size = smoke.getWidth() - 0.75f;
+            smoke.setSize(size, size);
+            smoke.setOrigin(size / 2f, size / 2f);
+            smoke.setX(smoke.getX() + 0.005f);
+            smoke.setY(smoke.getY() + 0.005f);
+            smoke.rotateBy((float) Math.toDegrees(Math.PI / 2f + Math.PI / 2f * Math.random()));
+        }
+
+        if (!smokeList.isEmpty() && smokeList.get(0).getWidth() < 0.51f) {
+            smokeList.get(0).remove();
+            smokeList.remove(0);
+            smokeAlpha.remove(0);
+        }
     }
+
 
     @Override
     public void render(float delta) {
